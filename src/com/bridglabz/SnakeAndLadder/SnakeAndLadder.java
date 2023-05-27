c965ca102cd87e5c72762a588a8a08305e30d1aa
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-27 10:49:48 +0530

    Added : player start position zero

---
+++ src/com/bridglabz/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridglabz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int player=0;
+        final int START_POSITION=0;
+        System.out.println("Welcome to snake and ladder game");
+    }
+}
