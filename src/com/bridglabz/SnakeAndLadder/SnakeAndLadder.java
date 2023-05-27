a9de5787e165affbeed6d20e06a98cc0bd9725eb
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-27 11:08:24 +0530

    Added : Random-number-between-one-to-six

--- src/com/bridglabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
         int player=0;
         final int START_POSITION=0;
         System.out.println("Welcome to snake and ladder game");
+        int dice=(int)(Math.random()*6)+1;
+        System.out.println(dice);
     }
 }
